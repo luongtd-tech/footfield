97f7ad6c3ce3936543b9fa3191b39f94744b4f55
luongtd-tech <luongtd.tech@gmail.com>
2026-04-24 04:06:47 +0700

    fix deploy

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST || '127.0.0.1',
-      port: process.env.DB_PORT || 3306,
+      port: Number(process.env.DB_PORT) || 3306,
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || '',
       database: process.env.DB_NAME || 'footfield'
@@ -27,7 +27,7 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: Number(process.env.DB_PORT) || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
