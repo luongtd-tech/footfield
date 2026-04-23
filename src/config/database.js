773ff375ef5f4399ad078617b0924c707315241c
luongtd-tech <luongtd.tech@gmail.com>
2026-04-24 03:56:34 +0700

    fix deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const config = {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+};
+
+// Thêm cấu hình SSL nếu chạy trên Production (Aiven yêu cầu SSL)
+if (process.env.NODE_ENV === 'production') {
+  config.ssl = {
+    rejectUnauthorized: false
+  };
+}
+
+const pool = mysql.createPool(config);
 
 module.exports = pool.promise();
